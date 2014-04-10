9aa874efcc1949305f9cf8820610decf13390dcc
rmarinov <chessmasterrado@gmail.com>
2014-04-10 01:00:20 -0500

    Fixed Largest prime factor

--- largestPrime/src/prime.java
+++ largestPrime/src/prime.java
@@ -7,18 +7,27 @@ import java.math.*;
 public class prime {public static void main(String[] args){
     long testNum = 600851475143L;
 //    long testNum = 13195;
+//    long testNum = 14;
+
 
     long div = (long)Math.sqrt((double)testNum);
 
     while(div > 1){
         if (testNum % div == 0){
             if(isPrime(div)){
-                System.out.println("The largest prime factor of the number " + testNum + " is " + div);
                 break;
             }
         }
         div--;
-    }  
+    }
+
+    if(isPrime(testNum/div)){
+        if (testNum/div > div){
+            div = testNum/div;
+        }
+    }
+    System.out.println("The largest prime factor of the number " + testNum + " is " + div);
+
 }
     public static boolean isPrime(long num){
         if(num == 1) return false;
---
+++ palProd/src/palProd.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Rado on 4/1/14.
+ */
+public class palProd {    public static void main(String[] args){
+
+
+//    boolean isPal = false;
+//
+//    for(int i = 0; i < i2; i++){
+//        if( test[i] != test[(int)len-i-1]){
+//            break;
+//        }
+//        if (i >= i2-1){
+//            isPal = true;
+//        }
+//    }
+
+}
+}
+
